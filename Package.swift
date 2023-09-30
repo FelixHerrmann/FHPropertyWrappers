3981a374501c2a0f726bd6cea4fbd968813cafef
Felix Herrmann <felix.herrmann@icloud.com>
2023-09-30 12:58:50 +0200

    Fix build

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         .target(
             name: "FHPropertyWrappers",
             dependencies: [],
-            resources: [.copy("Resources")]
+            resources: [.process("Resources")]
         ),
         .testTarget(
             name: "FHPropertyWrappersTests",
